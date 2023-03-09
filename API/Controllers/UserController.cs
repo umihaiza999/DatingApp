95a4474cbd9d82d7e58262d2b304609af0eaf59f
DESKTOP-SRMQ8AK\fmasm <umi.mahamud@fsoft.com.vn>
2023-03-09 13:36:55 +0800

    End of section 2

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controller
+{
+    [ApiController]
+   [Route("api/[controller]")]
+    public class UserController
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+         public async Task<ActionResult<AppUser>> GetUsers(int id)
+        { 
+            return await _context.Users.Find(id);
+        }
+    }
+}
